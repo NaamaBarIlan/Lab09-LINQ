97c97ee396246734d415d8d469d58bd831e9991a
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-07-16 16:59:19 -0700

    Created new classes,converted the JSON data, deserialized the data. Question 1 has correct output.

--- Lab09-LINQ/Program.cs
+++ Lab09-LINQ/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using Lab09_LINQ.Classes;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace Lab09_LINQ
 {
     class Program
     {
+        public static Root JsonData { get; set; }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            DeserializeJson();
+        }
+
+        static public void DeserializeJson()
+        {
+            string rawData = File.ReadAllText("../../../Data.JSON");
+
+            // From the newtonsoft docs:
+            // Product deserializedProduct = JsonConvert.DeserializeObject<Product>(output);
+            JsonData = JsonConvert.DeserializeObject<Root>(rawData);
+            int counter = 1;
+
+            Console.WriteLine("All of the neighborhoods in the data list:");
+            foreach (var item in JsonData.features)
+            {
+                Console.WriteLine($"{counter}. {item.properties.neighborhood}");
+                counter++;
+            }
         }
     }
+ 
 }
