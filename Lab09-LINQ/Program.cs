110859067825ba29777308e40c94c6dc52b0bf19
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-07-16 18:48:23 -0700

    Updated README and added images

--- Lab09-LINQ/Program.cs
+++ Lab09-LINQ/Program.cs
@@ -15,25 +15,50 @@ namespace Lab09_LINQ
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            DeserializeJson();
+            OutputAll();
         }
 
-        static public void DeserializeJson()
+        static public Root DeserializeJson()
         {
             string rawData = File.ReadAllText("../../../Data.JSON");
 
             // From the newtonsoft docs:
             // Product deserializedProduct = JsonConvert.DeserializeObject<Product>(output);
             JsonData = JsonConvert.DeserializeObject<Root>(rawData);
+
+            return JsonData;
+        }
+
+        static public void OutputAll()
+        {
+            JsonData = DeserializeJson();
             int counter = 1;
 
             Console.WriteLine("All of the neighborhoods in the data list:");
+
+            var allNeighborhoods = from place in JsonData.features
+                                   select place.properties.neighborhood;
+
+
             foreach (var item in JsonData.features)
             {
                 Console.WriteLine($"{counter}. {item.properties.neighborhood}");
                 counter++;
             }
+
         }
-    }
+
+        /*
+        static public void FilterData()
+        {
+            JsonData = DeserializeJson();
+            // Filtering
+            /*
+            var filter  = from person in persons
+                          where person.Age > 21
+                          select new { person.FirstName, person.LastName };
+
+        */
+        }
  
 }
